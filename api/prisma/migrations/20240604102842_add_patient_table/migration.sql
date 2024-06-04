bc72deafec0dc44838c83ff934afa435d6882ecb
Germano Ricardi <germanoricardi7@gmail.com>
2024-06-04 06:29:00 -0400

    Criado migration para tabela patient

---
+++ api/prisma/migrations/20240604102842_add_patient_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Patient" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Patient_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Patient_userId_key" ON "Patient"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Patient_name_key" ON "Patient"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Patient_phone_key" ON "Patient"("phone");
